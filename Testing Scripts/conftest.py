107823d5ac269109d4012e30f288c5a1d3b179e2
ProboticsX <jainshubham0403@gmail.com>
2023-04-18 13:44:22 -0400

    Added options

--- Testing Scripts/conftest.py
+++ Testing Scripts/conftest.py
@@ -1,11 +1,23 @@
 import pytest
 
 @pytest.fixture(scope = "session", autouse = True)
-def tc_setup():
-    print("Launch Browser")
+def tc_setup(browser):
+    if browser == "chrome":
+        print("Launch Chrome")
+    else:
+        print("Launch Browser")
     yield
-    print("Close Browser")
+    if browser == "chrome":
+        print("Close Chrome")
+    else:
+        print("Close Browser")
+@pytest.fixture(scope = "session", autouse = True)
+def browser(request):
+    return request.config.getoption("--browser")
+def pytest_addoption(parser):
+    parser.addoption("--browser")
 
 @pytest.fixture(params=["a","b"])
 def param_setup(request):
     print (request.param)
+
