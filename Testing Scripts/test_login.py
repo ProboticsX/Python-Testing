e5dfbc82cc8201764b2e5171c36da048b85eca33
ProboticsX <jainshubham0403@gmail.com>
2023-04-18 12:59:51 -0400

    Markers added

--- Testing Scripts/test_login.py
+++ Testing Scripts/test_login.py
@@ -1,8 +1,8 @@
 import pytest
-
-@pytest.mark.sanity
+import sys
+@pytest.mark.xfail(sys.platform == "win64", reason="Can't work on windows 64 bit")
 def test_login():
     print("Login Success!")
-
+@pytest.mark.skipif(sys.version_info < (4,3),reason="requires python4.3")
 def test_logout():
     print("Logout Success!")
