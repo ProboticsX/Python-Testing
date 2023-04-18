5bf478bffba2bd5c0275cb1a5d0547d26deddf37
ProboticsX <jainshubham0403@gmail.com>
2023-04-18 13:17:24 -0400

    Parameterizing Fixtures

--- Testing Scripts/conftest.py
+++ Testing Scripts/conftest.py
@@ -5,3 +5,7 @@ def tc_setup():
     print("Launch Browser")
     yield
     print("Close Browser")
+
+@pytest.fixture(params=["a","b"])
+def param_setup(request):
+    print (request.param)
--- Testing Scripts/test_login.py
+++ Testing Scripts/test_login.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
 @pytest.mark.xfail(sys.platform == "win64", reason="Can't work on windows 64 bit")
-def test_login():
+def test_login(param_setup):
     print("Login Success!")
 @pytest.mark.skipif(sys.version_info < (3,3),reason="requires python4.3")
 def test_logout():
